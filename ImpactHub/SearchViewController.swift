072bc48b52ae7ebf76246737d19ce7838163bf32
Niklas Alvaeus <niklas@helloniklas.com>
2017-08-02 10:46:23 +0100

    Added global search bar

--- ImpactHub/SearchViewController.swift
+++ ImpactHub/SearchViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import PromiseKit
+import SalesforceSDKCore
 
-class SearchViewController: UIViewController {
+
+class SearchViewController: ListWithSearchViewController, CreatePostViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,5 +33,125 @@ class SearchViewController: UIViewController {
         self.navigationController?.navigationBar.barStyle = .default
         
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.searchBar?.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.cancelSearching()
+    }
+    
+    var selectedVM: MemberViewModel?
+    var memberToSendMessage: Member?
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: self)
+        if segue.identifier == "ShowMember" {
+            if let vc = segue.destination as? MemberViewController, let selectedItem = selectedVM {
+                vc.member = selectedItem.member
+            }
+        }
+        else if segue.identifier == "ShowCreatePost" {
+            if let navVC = segue.destination as? UINavigationController {
+                if let vc = navVC.viewControllers.first as? CreatePostViewController, let contactId = cellWantsToSendContactRequest?.vm?.member.id {
+                    vc.delegate = self
+                    vc.createType = .contactRequest(contactId: contactId)
+                }
+            }
+        }
+        else if segue.identifier == "ShowMessageThread" {
+            if let vc = segue.destination as? MessagesThreadViewController, let member = self.memberToSendMessage {
+                vc.member = member
+            }
+        }
+    }
+    
+    func didCreatePost(post: Post) {
+        
+    }
+    
+    func didCreateComment(comment: Comment) {
+        
+    }
+    
+    func didSendContactRequest() {
+        self.cellWantsToSendContactRequest?.connectRequestStatus = .outstanding
+    }
+    
+    var cellWantsToSendContactRequest: MemberCollectionViewCell?
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
+    }
+    
+    
+    
+    
+    // MARK: Search
+    override func filterContentForSearchText(dataToFilter: [CellRepresentable], searchText:String) -> [CellRepresentable] {
+        return dataToFilter.filter({ (item) -> Bool in
+            if let vm = item as? MemberViewModel {
+                return vm.member.name.lowercased().contains(searchText.lowercased()) || vm.member.locationName.lowercased().contains(searchText.lowercased())
+            }
+            else {
+                return false
+            }
+        })
+    }
+}
+
+
+extension SearchViewController: MemberCollectionViewCellDelegate {
+    
+    func wantsToCreateNewMessage(member: Member) {
+        self.memberToSendMessage = member
+        self.performSegue(withIdentifier: "ShowMessageThread", sender: self)
+    }
+    
+    
+    func wantsToSendContactRequest(member: Member, cell: MemberCollectionViewCell) {
+        self.cellWantsToSendContactRequest = cell
+        self.performSegue(withIdentifier: "ShowCreatePost", sender: self)
+    }
+    
+}
+
+
+extension SearchViewController {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let vm = data[indexPath.item] as? MemberViewModel {
+            selectedVM = vm
+            performSegue(withIdentifier: "ShowMember", sender: self)
+        }
+    }
+}
+
+
+
+extension SearchViewController {
+    
+    override func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        
+        guard let indexPath = collectionView.indexPathForItem(at: location) else { return nil }
+        guard let cell = collectionView.cellForItem(at: indexPath) else { return nil }
+        
+        var detailVC: UIViewController!
+        
+        if let vm = data[indexPath.item] as? MemberViewModel {
+            selectedVM = vm
+            detailVC = storyboard?.instantiateViewController(withIdentifier: "MemberViewController")
+            (detailVC as! MemberViewController).member = selectedVM?.member
+            
+            //        detailVC.preferredContentSize = CGSize(width: 0.0, height: 300)
+            previewingContext.sourceRect = cell.frame
+            
+            return detailVC
+        }
+        
+        return nil
+        
+    }
 }
